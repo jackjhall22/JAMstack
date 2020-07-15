66bdf22aebeaf84c69100d522fc48436fb2bc817
jackjhall22 <jackjhall22@me.com>
2020-07-15 11:49:22 -0700

    feat(dashboard): add dashboard.js to pages

---
+++ sections/dashboard/src/pages/dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      <p>TODO: create a dashboard</p>
+    </Layout>
+  )
+}
+
+export default Dashboard;
